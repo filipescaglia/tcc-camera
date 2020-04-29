17476bed652723ed8345f37a4bead9176173420a
Filipe Scaglia <filipecscaglia@gmail.com>
2020-04-28 22:57:49 -0300

    bug fixes

--- main.py
+++ main.py
@@ -9,17 +9,17 @@ sys.path.append('./dependencies')
 
 from detection import detection
 
-def send_alert(camera_id, frame):
+def send_alert(camera_id, image, frame):
     try:
         
-        url = 'http://192.168.15.10:8000/api/alert'
+        url = 'http://192.168.15.21:8000/api/alert'
 
         headers = { 'content-type': "multipart/form-data",
                     'accept': "application/json" }
 
         data = { 'camera_id': camera_id, 'has_human': 1 }
 
-        file = { 'image': ('image.jpeg', frame.tostring(), 'image/jpeg') }
+        file = { 'image': (image, frame.tostring(), 'image/jpeg') }
 
         response = requests.post(url, data=data, files=file)
 
@@ -37,15 +37,15 @@ def main():
     camera = PiCamera()
 
     while True:    
-    image = getFileName() # Gets a filename
-    pir.wait_for_motion() # Waits for motion on the sensor
-    print("Movimento detectado") # Prints 'Movimento detectado' on terminal
-    #camera.start_preview() # Starts camera live preview
-    camera.capture(filename) # Gets the image from camera
-    #camera.stop_preview() # Stops camera live preview
-    confidence, frame = detection(image)
-    if confidence != None:
-        send_alert(1, frame)
-    time.sleep(5) # Waits 5 seconds for another work cycle
+        image = getFileName() # Gets a filename
+        pir.wait_for_motion() # Waits for motion on the sensor
+        print("Movimento detectado") # Prints 'Movimento detectado' on terminal
+        #camera.start_preview() # Starts camera live preview
+        camera.capture(image) # Gets the image from camera
+        #camera.stop_preview() # Stops camera live preview
+        confidence, frame = detection(image)
+        if confidence != None:
+            send_alert(1, image, frame)
+        time.sleep(5) # Waits 5 seconds for another work cycle
 
 main()
